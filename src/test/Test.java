70e2cd1ff8b58709337ba72b880087bfc67fcff3
gaosongm <41891894+gaosongm@users.noreply.github.com>
2018-07-31 14:50:26 +0800

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,34 +1,10 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class Test {
+
+public class Test1 {
 	
 	public static void main(String[] args) {
-		String str = "pk_user in (select cuserid from sm_user where pk_org in ('0001A110000000000A2Y','0001A110000000002DQ9')) AND tempstyle in ('单据模板','查询模板') AND pk_user in ('0001A11000000000288Y','0001A110000000002DNJ')";
-	    String[] values = str.split("AND");	
-	    String[] result = null;
-	    
-	    Map<String,String[]> map = new HashMap<String,String[]>();
-	    
-	    //增加一行描述测试一下
-	    Pattern p = Pattern.compile("\'.*\'");
-	    Matcher m = p.matcher(values[0]);
-	    List<String> list = new ArrayList<String>();
-	    int index = 0;
-	    String pk = null;
-	    while(m.find()){
-	    	pk = m.group(0);
-	    	list.add(pk.replace("\'", ""));
-//	    	index++;
-	    }
-	    System.out.println(list);
-		
+		System.out.println(“公有");
 	}
-
 }
